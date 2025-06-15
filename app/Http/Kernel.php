321b54b2ee66789cd4c9bb8b77bdfc46e778998f
fadlibdr <143300758+fadlibdr@users.noreply.github.com>
2025-06-16 04:40:19 +0700

    Rename route middleware property

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
     /**
      * Route middleware.
      */
-    protected $routeMiddleware = [
+    protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
