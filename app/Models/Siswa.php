1ff279b3340eac4ff9109fae69edcf887aa36f62
fadlibdr <143300758+fadlibdr@users.noreply.github.com>
2025-06-16 05:29:59 +0700

    Revert "feat: auto create student user accounts"
    This reverts commit d81fe996611bcb6b72bfdb937a752c469e97ab0a.

--- app/Models/Siswa.php
+++ app/Models/Siswa.php
@@ -6,8 +6,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Kelas;
 use App\Models\Iuran;
-use App\Models\User;
-use Illuminate\Support\Carbon;
 
 class Siswa extends Model
 {
@@ -47,23 +45,5 @@ class Siswa extends Model
     public function iuran()
     {
         return $this->hasMany(Iuran::class, 'siswa_id');
-    }
-
-    protected static function booted()
-    {
-        static::created(function (Siswa $siswa) {
-            $birth = $siswa->tanggal_lahir
-                ? Carbon::parse($siswa->tanggal_lahir)->format('dmy')
-                : '';
-            $password = $siswa->nama_depan . $birth;
-
-            $user = User::create([
-                'name' => $siswa->nama_depan . ' ' . $siswa->nama_belakang,
-                'email' => $siswa->email,
-                'password' => $password,
-            ]);
-
-            $user->assignRole('siswa');
-        });
     }
 }
