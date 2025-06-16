ac2f1ccc44158b9ca0d606a131d8ef7fe29b55c4
fadlibdr <fadlibadar@gmail.com>
2025-06-16 10:20:05 +0700

    Changes to be committed: 	modified:   resources/views/partials/sidebar.blade.php

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -95,15 +95,13 @@
                 <a class="collapse-item" href="{{ route('iuran.index') }}">Iuran</a>
                 <a class="collapse-item" href="{{ route('keuangan.index') }}">Keuangan</a>
                 <a class="collapse-item" href="{{ route('riwayat.index') }}">Riwayat Pembayaran</a>
-                <a class="collapse-item" href="{{ route('cek-pembayaran.index') }}">Cek Pembayaran</a>
+                <a class="collapse-item" href="{{ route('cek-pembayaran.index') }}">Tagihan</a>
                 <a class="collapse-item" href="{{ route('jurnal-umum.index') }}">Jurnal Umum</a>
-                <a class="collapse-item" href="{{ route('users.index') }}">User Management</a>
-                <a class="collapse-item" href="{{ route('settings.index') }}">Application Settings</a>
-
-
             </div>
         </div>
     </li>
+
+    @role('admin')
     <!-- Divider -->
     <hr class="sidebar-divider">
 
@@ -114,22 +112,21 @@
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseComponent"
-            aria-expanded="true" aria-controls="collapseComponent">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapse" aria-expanded="true"
+            aria-controls="collapse">
             <i class="fas fa-fw fa-wrench"></i>
             <span>Laporan</span>
-
-            <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-                data-parent="#accordionSidebar">
-                <div class="bg-white py-2 collapse-inner rounded">
-                    <h6 class="collapse-header">Settings</h6>
-                    <a class="collapse-item" href="{{ route('users.index') }}">User Management</a>
-                    <a class="collapse-item" href="{{ route('settings.index') }}">Application Settings</a>
-                </div>
+        </a>
+        <div id="collapse" class="collapse" aria-labelledby="heading" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Settings</h6>
+                <a class="collapse-item" href="{{ route('users.index') }}">User Management</a>
+                <a class="collapse-item" href="{{ route('settings.index') }}">Application Settings</a>
             </div>
+        </div>
     </li>
 
-    @role('admin')
+
     <li class="nav-item">
         <a class="nav-link" href="{{ route('users.index') }}">
             <i class="fas fa-users-cog"></i>
